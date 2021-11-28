e93aa281ed81df1ef3a610a7e5b71bbfc8a630cb
unknown <marcin.kluska@edu.uekat.pl>
2021-11-28 10:40:00 +0100

    zadania

--- magazine/product.py
+++ magazine/product.py
@@ -1,12 +1,1 @@
-from magazine.utils import Property
-
-
-class House(Property):
-    def __init__(self, area, rooms: int, price, address, rozmiar_dzialki: int):
-        super().__init__(area, rooms, price, address)
-        self.rozmiar_dzialki: int = rozmiar_dzialki
-
-    def __str__(self):
-        return super() \
-                   .__str__() + ' rozmiar dizalki : {}\n' \
-                   .format(self.rozmiar_dzialki)
+import magazine.utils
--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from magazine.product import House
-from magazine.order import Flat
-
-house1 = House(7, 2, 3, 'adres', 39)
-flat1 = Flat(2, 5, 30, 'adress', 3)
-
-print(house1)
-print(flat1)
+import  magazine.product
