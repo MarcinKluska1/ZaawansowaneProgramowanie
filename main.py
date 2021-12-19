ba52ff4080c40b55289d61a1ccb37a913560c914
Marcin Kluska <marcin.kluska@edu.uekat.pl>
2021-12-19 14:04:38 +0100

    tak

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 def readPictureText(imagePath: str) -> str:
     image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.threshold(cv2.GaussianBlur(image, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    image = cv2.threshold(cv2.bilateralFilter(image, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    image = cv2.threshold(cv2.medianBlur(image, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    image = cv2.adaptiveThreshold(cv2.GaussianBlur(image, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    image = cv2.adaptiveThreshold(cv2.bilateralFilter(image, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    image = cv2.adaptiveThreshold(cv2.medianBlur(image, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
     return pytesseract.image_to_string(image, lang='eng').replace('\n', '')
 
 
-imagePaths: list[str] = ['images/bozenka.jpg',
+imagePaths: list[str] = [
+                         'images/bozenka.jpg',
                          'images/ulani.jpg',
                          'images/kot.jpg',
                          'images/czerwone.jpg',
                          'images/ulica.jpg',
                          'images/man.png',
-                         'images/street.jpg',]
+                         'images/street.jpg',
+                         'images/captcha1.png',
+                         'images/captcha2.png',
+                         'images/captcha3.png',
+                         'images/captcha4.png',]
 
 for imagePath in imagePaths:
     print('----------------------------')
